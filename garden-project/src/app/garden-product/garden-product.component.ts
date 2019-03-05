e5940bd151cde629d727b44d54c33452d481c635
Kate Stagner <katestagner4@gmail.com>
2019-03-05 15:29:02 -0500

    added components

---
+++ garden-project/src/app/garden-product/garden-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-garden-product',
+  templateUrl: './garden-product.component.html',
+  styleUrls: ['./garden-product.component.css']
+})
+export class GardenProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
