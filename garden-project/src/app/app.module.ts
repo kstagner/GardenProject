b365e6644c2c376a18b5d1722460a8a4803c17dc
Kate Stagner <katestagner4@gmail.com>
2019-03-06 11:55:01 -0500

    finished home page

--- garden-project/src/app/app.module.ts
+++ garden-project/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GardenHomeComponent } from './garden-home/garden-home.component';
 import { GardenAboutComponent } from './garden-about/garden-about.component';
 import { GardenProductComponent } from './garden-product/garden-product.component';
 
+const routes = [
+  { path: 'about', component: GardenAboutComponent },
+  { path: 'products', component: GardenProductComponent },
+  { path: 'home', component: GardenHomeComponent },
+  //{ path: 'login', component: }
+
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +25,9 @@ import { GardenProductComponent } from './garden-product/garden-product.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
